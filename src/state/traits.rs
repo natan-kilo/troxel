cef3484dab466ac9ce7b83b4e525a6ea5f06b608
Sebastian <Sebastian@Strobl.net>
2020-06-06 22:44:33 +0200

    Add: Associatied Function Parameters

--- src/state/traits.rs
+++ src/state/traits.rs
@@ -2,9 +2,10 @@ use std::any::Any;
 use std::ops::Index;
 
 pub trait Stateful: Any {
-    fn render(&mut self);
-    fn update(&mut self);
-    fn input(&mut self);
+    fn render(&mut self, frame: &wgpu::SwapChainOutput, encoder: &mut wgpu::CommandEncoder);
+    fn update(&mut self, device: &mut wgpu::Device, queue: &mut wgpu::Queue);
+    fn input(&mut self, event: &winit::event::WindowEvent) -> bool;
+    fn resize(&mut self, device: &mut wgpu::Device, sc_desc: &mut wgpu::SwapChainDescriptor, size: &winit::dpi::PhysicalSize<u32>);
     fn id(&self) -> usize;
     fn box_eq(&self, other: &dyn Any) -> bool;
     fn as_any(&self) -> &dyn Any;
